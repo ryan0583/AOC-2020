d225a1544af15ecfc55c10d8868ddf40fc0dc993
Ryan Griffiths <ryan0583@gmail.com>
2019-12-04 23:49:32 +0000

    fixing unit test name

--- Day 4/day4_test.py
+++ Day 4/day4_test.py
@@ -8,32 +8,32 @@ class Test(unittest.TestCase):
     def test_part_one(self):
             self.assertEqual(1748, run(part_one))
 
-    def test_part_one(self):
+    def test_part_two(self):
         self.assertEqual(1180, run(part_two))
 
-    # def test1(self):
-    #     self.assertTrue(meets_criteria_two("112233"))
-    #
-    # def test2(self):
-    #     self.assertFalse(meets_criteria_two("123444"))
-    #
-    # def test3(self):
-    #     self.assertFalse(meets_criteria_two("123443"))
-    #
-    # def test4(self):
-    #     self.assertTrue(meets_criteria_two("112222"))
-    #
-    # def test5(self):
-    #     self.assertTrue(meets_criteria_two("577788"))
-    #
-    # def test6(self):
-    #     self.assertFalse(meets_criteria_two("111111"))
-    #
-    # def test7(self):
-    #     self.assertFalse(meets_criteria_two("122222"))
-    #
-    # def test8(self):
-    #     self.assertFalse(meets_criteria_two("234444"))
+    def test1(self):
+        self.assertTrue(meets_criteria_two("112233"))
+
+    def test2(self):
+        self.assertFalse(meets_criteria_two("123444"))
+
+    def test3(self):
+        self.assertFalse(meets_criteria_two("123443"))
+
+    def test4(self):
+        self.assertTrue(meets_criteria_two("112222"))
+
+    def test5(self):
+        self.assertTrue(meets_criteria_two("577788"))
+
+    def test6(self):
+        self.assertFalse(meets_criteria_two("111111"))
+
+    def test7(self):
+        self.assertFalse(meets_criteria_two("122222"))
+
+    def test8(self):
+        self.assertFalse(meets_criteria_two("234444"))
 
 
 if __name__ == '__main__':
