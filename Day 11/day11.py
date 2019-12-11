37387e34dc06e6249b6abc1d8d25f3efc6cd996b
Ryan Griffiths <ryan0583@gmail.com>
2019-12-11 06:52:06 +0000

    Day 11 part 1

---
+++ Day 11/day11.py
@@ -0,0 +1,89 @@
+from Utils.debug_tools import raise_
+from Utils.intcode_computer import IntcodeComputer
+from Utils.point import Point
+
+
+class Robot:
+    def __init__(self, filename):
+        self.brain = IntcodeComputer([], filename, True)
+        self.position = Point(0, 0)
+        self.direction = UP
+
+    def turn(self, rotation):
+        if rotation == ROTATE_RIGHT:
+            self.direction = self.direction + 1
+            if self.direction > LEFT:
+                self.direction = UP
+        else:
+            self.direction = self.direction - 1
+            if self.direction < UP:
+                self.direction = LEFT
+
+    def move(self):
+        if self.direction == UP:
+            self.position = Point(self.position.x, self.position.y - 1)
+        elif self.direction == RIGHT:
+            self.position = Point(self.position.x + 1, self.position.y)
+        elif self.direction == DOWN:
+            self.position = Point(self.position.x, self.position.y + 1)
+        elif self.direction == LEFT:
+            self.position = Point(self.position.x - 1, self.position.y)
+        else:
+            raise_(Exception("Unrecognised direction " + str(self.direction)))
+
+
+class Panel:
+    def __init__(self):
+        self.colours = {}
+
+    def paint(self, color, position):
+        self.colours[str(position.x) + ", " + str(position.y)] = color
+
+    def get_colour(self, position):
+        colour = self.colours.get(str(position.x) + ", " + str(position.y))
+        return colour if colour is not None else BLACK
+
+
+def part1():
+    robot = Robot("input.txt")
+    panel = Panel()
+
+    while robot.brain.is_running():
+        next_input = panel.get_colour(robot.position)
+        robot.brain.append_input(next_input)
+        next_colour = robot.brain.process()
+        next_rotation = robot.brain.process()
+        panel.paint(next_colour, robot.position)
+        robot.turn(next_rotation)
+        robot.move()
+
+    print(len(panel.colours))
+
+def part1():
+    robot = Robot("input.txt")
+    panel = Panel()
+
+    while robot.brain.is_running():
+        next_input = panel.get_colour(robot.position)
+        robot.brain.append_input(next_input)
+        next_colour = robot.brain.process()
+        next_rotation = robot.brain.process()
+        panel.paint(next_colour, robot.position)
+        robot.turn(next_rotation)
+        robot.move()
+
+    print(len(panel.colours))
+
+
+BLACK = 0
+WHITE = 1
+
+ROTATE_LEFT = 0
+ROTATE_RIGHT = 1
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+part1()
