c3c4fab609547079424634e8ddd07c45432ddb2b
Ryan Griffiths <Ryan.Griffiths@bjss.com>
2019-12-11 08:54:16 +0000

    adding file parser

---
+++ Utils/file_parser.py
@@ -0,0 +1,14 @@
+from Utils.point import Point
+
+
+class FileParser:
+    def __init__(self, filename):
+        self.lines = open(filename, "r").read().splitlines()
+
+    def read_points(self, char):
+        points = []
+        for y, line in enumerate(self.lines):
+            for x, this_char in enumerate(line):
+                if this_char == char:
+                    points.append(Point(x, y))
+        return points
