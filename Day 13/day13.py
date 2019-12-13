cc6f787f597079d3a84d7357734ad49eaacd8781
Ryan Griffiths <Ryan.Griffiths@bjss.com>
2019-12-13 13:10:08 +0000

    Code cleanup

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -28,9 +28,9 @@ def part2():
         if last_ball_point is not None:
             moving_right = last_ball_point.x < point.x
             if moving_right:
-                _joystick_tilt = JOY_RIGHT
+                return JOY_RIGHT
             else:
-                _joystick_tilt = JOY_LEFT
+                return JOY_LEFT
 
         return _joystick_tilt
 
@@ -42,64 +42,26 @@ def part2():
     last_ball_point = None
     paddle_point = None
     joystick_tilt = JOY_CENTRAL
-    has_drawn_block = False
-    setup_complete = False
-    grid = None
 
     while intcode_computer.is_running():
         x = intcode_computer.process()
         y = intcode_computer.process()
         output = intcode_computer.process()
         if x == SCORE_X and y == SCORE_Y:
-            # print("SCORE: " + str(output))
-            # print("BLOCK_COUNT: " + str(sum(map(BLOCK.__eq__, tiles.values()))))
             score = output
-            setup_complete = True
-            if has_drawn_block:
-                if sum(map(BLOCK.__eq__, tiles.values())) == 0:
-                    break
+            if sum(map(BLOCK.__eq__, tiles.values())) == 0:
+                break
         else:
             point = Point(x, y)
 
-            if output == BLOCK:
-                # print("BLOCK: " + str(point))
-                has_drawn_block = True
-            # elif output == WALL:
-                # print("WALL: " + str(point))
-            # elif output == EMPTY:
-                # print("EMPTY: " + str(point))
-            elif output == BALL:
-                # print("BALL: " + str(point))
+            if output == BALL:
                 joystick_tilt = get_joystick_tilt(joystick_tilt)
                 last_ball_point = point
             elif output == PADDLE:
                 paddle_point = point
-                # print("PADDLE: " + str(point))
 
             tiles[point] = output
 
-            if setup_complete:
-                if grid is None:
-                    x_dim = max(list(map(lambda _point: _point.x, tiles.keys()))) + 1
-                    y_dim = max(list(map(lambda _point: _point.y, tiles.keys()))) + 1
-                    grid = create_grid(x_dim, y_dim, "  ")
-
-                for point in tiles.keys():
-                    tile_type = tiles.get(point)
-                    tile_char = None
-                    if tile_type == EMPTY:
-                        tile_char = " "
-                    elif tile_type == WALL:
-                        tile_char = "|"
-                    elif tile_type == BLOCK:
-                        tile_char = "#"
-                    elif tile_type == PADDLE:
-                        tile_char = "_"
-                    elif tile_type == BALL:
-                        tile_char = "O"
-                    grid[point.y][point.x] = tile_char
-
-        # joystick_tilt = int(input("enter joystick movement:\n"))
         intcode_computer.replace_next_input(joystick_tilt)
 
     print(score)
