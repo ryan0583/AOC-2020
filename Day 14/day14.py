048c98111f3168c324aee338a6291b39353f90d0
Ryan Griffiths <ryan0583@gmail.com>
2019-12-15 14:53:23 +0000

    Day 14 Part 1 nearly working

--- Day 14/day14.py
+++ Day 14/day14.py
@@ -24,7 +24,7 @@ def consolidate_list(chem_list):
             list_map[chem_amount] = chem_amount
         else:
             existing_chem_amount.amount = existing_chem_amount.amount + chem_amount.amount
-    return list(list_map.values())
+    return list(filter(lambda value: value.amount > 0, list(list_map.values())))
 
 
 def create_chem_amounts(line):
@@ -36,30 +36,82 @@ def create_chem_amounts(line):
     return key, vals
 
 
-def expand_chem(chem_amount, chem_map):
+def adjust_for_surplus(chem_amount, surplus_chem):
+    chem_amount.amount = get_adjusted_chem_amount(chem_amount, surplus_chem, 1)
+
+
+def get_adjusted_chem_amount(chem_amount, surplus_chem, multiple):
+    print(chem_amount.chem)
+    print(str(chem_amount.amount))
+
+    surplus_chem_number = surplus_chem.get(chem_amount.chem)
+    print("SURPLUS: " + str(surplus_chem_number))
+    if surplus_chem_number is not None:
+        adjusted_chem_amount = multiple * chem_amount.amount - surplus_chem.get(chem_amount.chem, 0)
+        if adjusted_chem_amount < 0:
+            adjusted_chem_amount = 0
+        print("NEW CHEM AMOUNT: " + str(adjusted_chem_amount))
+        remaining = surplus_chem_number - chem_amount.amount
+        surplus_chem[chem_amount.chem] = remaining if remaining >= 0 else 0
+        print("NEW SURPLUS: " + str(surplus_chem[chem_amount.chem]))
+        return adjusted_chem_amount
+    print("NEW CHEM AMOUNT: " + str(multiple * chem_amount.amount))
+    return multiple * chem_amount.amount
+
+
+def expand_chem(chem_amount, chem_map, surplus_chem):
     if chem_amount == ORE:
         return [chem_amount]
+
+    if chem_amount.amount == 0:
+        return []
+
     chem_expansion = chem_map.get(chem_amount)
     multiple = math.ceil(chem_amount.amount / chem_expansion[0].amount)
+
+    surplus = multiple * chem_expansion[0].amount - chem_amount.amount
+    exiting_surplus = surplus_chem.get(chem_amount.chem)
+    if exiting_surplus is None:
+        surplus_chem[chem_amount.chem] = surplus
+    else:
+        surplus_chem[chem_amount.chem] = exiting_surplus + surplus
+    print("WILL HAVE SURPLUS OF: " + str(surplus_chem[chem_amount.chem]))
+
     return list(
-        map(lambda expanded_chem_amount: ChemAmount(multiple * expanded_chem_amount.amount, expanded_chem_amount.chem),
+        map(lambda expanded_chem_amount: ChemAmount(
+            get_adjusted_chem_amount(expanded_chem_amount, surplus_chem, multiple),
+            expanded_chem_amount.chem),
             chem_expansion[1]))
+
+
+def print_list(list):
+    output = ""
+    for chem_amount in list:
+        output += str(chem_amount.amount) + chem_amount.chem + ", "
+    print(output)
 
 
 def part1():
     lines = open("testinput.txt", "r").readlines()
     chem_map = {}
+    surplus_chem = {}
     for line in lines:
         key, vals = create_chem_amounts(line)
         chem_map[key] = key, vals
 
     fuel_chem_list = chem_map.get(FUEL)
+    print_list(fuel_chem_list[1])
     while len(fuel_chem_list[1]) > 1:
         expanded_list = []
         for chem_amount in fuel_chem_list[1]:
-            expanded_list.extend(expand_chem(chem_amount, chem_map))
+            adjust_for_surplus(chem_amount, surplus_chem)
+            if chem_amount.amount > 0:
+                expanded_list.extend(expand_chem(chem_amount, chem_map, surplus_chem))
+        print(surplus_chem)
+        print_list(expanded_list)
         chem_map[FUEL] = fuel_chem_list[0], consolidate_list(expanded_list)
         fuel_chem_list = chem_map.get(FUEL)
+        print_list(fuel_chem_list[1])
 
     print(fuel_chem_list[1][0].amount)
 
