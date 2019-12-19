6c620b80df8a8f4278a521d3cdee3c3c4241d7b8
Ryan Griffiths <Ryan.Griffiths@bjss.com>
2019-12-19 10:43:35 +0000

    day 19 part attempt not working

--- Day 19/day19.py
+++ Day 19/day19.py
@@ -1,3 +1,5 @@
+import math
+
 from Utils.graphics_panel import GraphicsPanel
 from Utils.intcode_computer import IntcodeComputer
 from Utils.point import Point
@@ -37,41 +39,54 @@ def part2():
 
     panel = GraphicsPanel.create_empty_panel(dim, dim)
     panel.init_canvas()
-    count = 0
 
     computer = IntcodeComputer([], None, False)
 
     filename = "input.txt"
     ints = list(map(int, open(filename, "r").read().split(",")))
 
-    x_next_start = 500
-    y_next_start = 300
-    y_last_col = []
+    points = []
 
-    while True:
-        panel.reset()
-        for x in range(x_next_start, x_next_start + dim):
-            for y in range(y_next_start, y_next_start + dim):
-                computer.reset(ints)
-                computer.inputs = [x, y]
-                computer.process()
-                computer.append_input(x)
-                computer.append_input(y)
-                output = computer.process()
-                if output == 1:
-                    count += 1
-                    panel.update_canvas_with_offset(Point(x, y), "red", - x_next_start, - y_next_start)
-                    panel.paint_canvas()
-                    if x == x_next_start + dim - 1:
-                        y_last_col.append(y)
-        if min(y_last_col) == y_next_start:
-            break
-        x_next_start += dim
-        y_next_start = min(y_last_col)
-        y_last_col = []
+    for x in range(0, dim):
+        for y in range(0, dim):
+            computer.reset(ints)
+            computer.inputs = [x, y]
+            output = computer.process()
+            if output == 1:
+                point = Point(x, y)
+                points.append(point)
+                panel.update_canvas(Point(x, y), "red")
+                panel.paint_canvas()
 
-    print(x_next_start, y_next_start)
-    input("press any key")
+    last_col_points = list(filter(lambda _point: _point.x == dim - 1, points))
+    drop = min(list(map(lambda _point: _point.y, last_col_points)))
+    beam_bottom = max(list(map(lambda _point: _point.y, last_col_points)))
+    angle1 = math.atan(drop / dim)
+    print(math.degrees(angle1))
+    angle2 = math.atan(beam_bottom / dim)
+    print(math.degrees(angle2))
+
+    x = ((dim / math.tan(angle2)) + (drop / math.tan(angle2))) / (1 - (math.tan(angle1) / math.tan(angle2)))
+    y = x * math.tan(angle1) + drop
+
+    print(str(x) + ", " + str(y))
+
+    start_x = 1509
+    start_y = math.floor(y)
+
+    panel.reset()
+    for x in range(start_x, start_x + dim):
+        for y in range(start_y, start_y + dim):
+            computer.reset(ints)
+            computer.inputs = [x, y]
+            output = computer.process()
+            if output == 1:
+                point = Point(x, y)
+                points.append(point)
+                panel.update_canvas_with_offset(Point(x, y), "red", -start_x, -start_y)
+                panel.paint_canvas()
+            else:
+                print("Output not 1 for point " + str(x) + ", " + str(y))
 
 
 part2()
