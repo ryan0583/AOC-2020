ccce1beec4cf9313a6ede2c9e7562b7b0283ce87
Ryan Griffiths <Ryan.Griffiths@bjss.com>
2019-12-20 09:00:56 +0000

    Day 20 part 1 not working

--- Day 20/day20.py
+++ Day 20/day20.py
@@ -10,43 +10,59 @@ class Droid:
         self.direction = direction
         self.portals = portals.copy()
 
-    def clone(self, direction):
-        return Droid(self.path, self.point, direction, self.portals)
+    def clone(self, direction, point):
+        return Droid(self.path, point, direction, self.portals)
 
-    def clone_reverse(self):
-        return self.clone(turn_right(turn_right(self.direction)))
-
-    def attempt_move(self, maze_map):
+    def attempt_move(self, maze_map, portal_map):
         def teleport():
-
+            key = get_teleport_key(next_char, next_point, maze_map)
+            if key is None:
+                return
+            if key in self.portals:
+                return
+            self.portals.add(key)
+            if key == "ZZ":
+                return
+            portal_points = portal_map.get(key)
+            teleport_point = None
+            for portal_point in portal_points:
+                if next_point not in portal_point:
+                    teleport_point = next(iter(portal_point))
+            if teleport_point is not None:
+                droids_to_add.append(self.clone(N, teleport_point))
+                droids_to_add.append(self.clone(S, teleport_point))
+                droids_to_add.append(self.clone(E, teleport_point))
+                droids_to_add.append(self.clone(W, teleport_point))
 
         def can_move_to_space(_next_char):
-            return _next_char is not None and (_next_char == SPACE or (maze_map[self.point].isupper() and _next_char.isupper))
+            return _next_char is not None and (
+                    _next_char == SPACE or (maze_map[self.point].isupper() and _next_char.isupper))
 
         droids_to_add = []
         droid_to_remove = None
 
         next_point = get_next_point(self.direction, self.point)
         next_char = maze_map.get(next_point)
-        if next_char is None:
+        if next_char is None or next_char == EMPTY:
             return droids_to_add, self
 
         can_move = can_move_to_space(next_char)
 
         if can_move:
+            if maze_map[self.point] == SPACE and next_char == SPACE:
+                self.path.append(self.point)
             self.point = next_point
-            self.path.append(self.point)
             can_turn_left = can_move_to_space(maze_map.get(get_next_point(turn_left(self.direction), self.point)))
             can_turn_right = can_move_to_space(maze_map.get(get_next_point(turn_right(self.direction), self.point)))
 
             if can_turn_left:
-                droids_to_add.append(self.clone(turn_left(self.direction)))
+                droids_to_add.append(self.clone(turn_left(self.direction), self.point))
 
             if can_turn_right:
-                droids_to_add.append(self.clone(turn_right(self.direction)))
+                droids_to_add.append(self.clone(turn_right(self.direction), self.point))
         elif next_char.isupper():
             droid_to_remove = self
-            print(get_teleport_key(next_char, next_point, maze_map))
+            teleport()
         else:
             droid_to_remove = self
 
@@ -108,19 +124,22 @@ def get_teleport_key(char, point, maze_map):
     point2 = None
     point2 = Point(point.x, point.y + 1)
     char2 = maze_map.get(point2)
-    if not char2.isupper():
+    if char2 is not None and not char2.isupper():
         point2 = Point(point.x, point.y - 1)
         char2 = maze_map.get(point2)
-    if not char2.isupper():
+    if char2 is not None and not char2.isupper():
         point2 = Point(point.x + 1, point.y)
         char2 = maze_map.get(point2)
-    if not char2.isupper():
+    if char2 is not None and not char2.isupper():
         point2 = Point(point.x - 1, point.y)
         char2 = maze_map.get(point2)
 
-    if point2.x < point.x or point2.y < point.y:
+    if char2 is None:
+        return None
+    elif point2.x < point.x or point2.y < point.y:
         return char2 + char
     return char + char2
+
 
 def part_one():
     def add_to_portal_map():
@@ -140,13 +159,13 @@ def part_one():
             print("found " + char + char2 + " at point " + str(point))
             point_list = portal_map.get(char + char2)
             if point_list is None:
-                portal_map[char + char2] = [[point, point2]]
+                portal_map[char + char2] = [{point, point2}]
             else:
-                point_list.append([point, point2])
+                point_list.append({point, point2})
 
-    lines = open("testinput.txt", "r").read().splitlines()
+    lines = open("input.txt", "r").read().splitlines()
 
-    panel = GraphicsPanel.create_empty_panel(100, 100)
+    panel = GraphicsPanel.create_empty_panel(200, 200)
     panel.init_canvas()
 
     maze_map = {}
@@ -163,27 +182,37 @@ def part_one():
 
     panel.paint_canvas()
 
-    start_point = portal_map.get("AA")[0][0]
+    start_point = next(iter(portal_map.get("AA")[0]))
 
     droids = [Droid([], start_point, N, set("AA")),
               Droid([], start_point, S, set("AA")),
               Droid([], start_point, E, set("AA")),
               Droid([], start_point, W, set("AA"))]
 
-    while True:
+    winning_droid = None
+
+    while winning_droid is None:
+        print("Droid count: " + str(len(droids)))
         droids_to_add = []
         droids_to_remove = set()
         for droid in droids:
             panel.update_canvas(droid.point, "black")
-            add, remove = droid.attempt_move(maze_map)
+            add, remove = droid.attempt_move(maze_map, portal_map)
             panel.update_canvas(droid.point, "blue")
             panel.paint_canvas()
+            if "ZZ" in droid.portals:
+                winning_droid = droid
+                break
             droids_to_add.extend(add)
             if remove is not None:
                 droids_to_remove.add(remove)
         droids.extend(droids_to_add)
         droids = list(filter(lambda this_droid: this_droid not in droids_to_remove, droids))
 
+    for point in winning_droid.path:
+        print(point)
+
+    print(len(winning_droid.path))
     input("press any key")
 
 
