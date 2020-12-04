2def3d2b74ba9f1cfb5033721aa5a652163a7011
Ryan Griffiths <ryan.griffiths@itv.com>
2020-12-04 07:07:33 +0000

    day 4 part 1 refactor

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,26 +1,18 @@
 import os
 
 
-def is_valid(line):
-    for field in required_fields:
-        if field not in line:
-            return False
-    return True
+def part1():
+    def is_valid(line):
+        for field in REQ_FIELDS:
+            if field not in line:
+                return False
+        return True
+
+    file = open("Input.txt", "r")
+    lines = list(file.read().split(os.linesep + os.linesep))
+    file.close()
+    return len(list(filter(lambda valid: valid, list(map(is_valid, lines)))))
 
 
-file = open("Input.txt", "r")
-lines = list(file.read().split(os.linesep + os.linesep))
-required_fields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
-print(len(lines))
-# print(lines[0])
-# print('==========')
-# print(lines[1])
-# print('==========')
-# print(lines[2])
-valid_count = 0
-for line in lines:
-    if is_valid(line):
-        valid_count = valid_count + 1
-
-print(valid_count)
-file.close()
+REQ_FIELDS = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
+print(part1())
