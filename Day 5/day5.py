fd0fe44fe294455085a6479f89daeb5c327c97c0
Ryan Griffiths <>
2020-12-06 22:22:25 +0000

    day 5 visualisation

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,8 +1,12 @@
+import time
+
 from Utils.file_reader import read_lines
+from Utils.graphics_panel import GraphicsPanel
+from Utils.point import Point
 
 
 def get_seat_id(line: str) -> int:
-    return int(line.replace('F', '0').replace('L', '0').replace('B', '1').replace('R', '1'), 2)
+    return int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
 
 
 def get_seat_ids() -> list:
@@ -13,11 +17,47 @@ def part1() -> int:
     return max(get_seat_ids())
 
 
+def convert_seat_id_to_point(seat_id: int) -> Point:
+    binary_seat_id = str(bin(seat_id))[2:].zfill(10)
+    x = int(binary_seat_id[0:7], 2)
+    y = int(binary_seat_id[7:len(binary_seat_id)], 2) + 2
+    if y > 3:
+        y += 1
+
+    if y > 8:
+        y += 1
+    return Point(x, y)
+
+
+def visualise(seat_ids: list, your_seat: int):
+    def take_seat():
+        graphics_panel.update_canvas(point, "blue")
+        graphics_panel.paint_canvas()
+        time.sleep(0.005)
+
+    points = list(map(convert_seat_id_to_point, seat_ids))
+
+    graphics_panel = GraphicsPanel.create_empty_panel(max(list(map(lambda point: point.x, points))) + 4,
+                                                      max(list(map(lambda point: point.y, points))) + 4,
+                                                      10)
+
+    graphics_panel.init_canvas()
+    graphics_panel.paint_canvas()
+    for point in points:
+        take_seat()
+
+    graphics_panel.update_canvas(convert_seat_id_to_point(your_seat), "yellow")
+    graphics_panel.paint_canvas()
+
+
 def part2() -> int:
     seat_ids = get_seat_ids()
-    return list(filter(lambda seat_id: seat_id not in seat_ids, list(range(min(seat_ids), max(seat_ids))))).pop()
+    your_seat = list(filter(lambda seat_id: seat_id not in seat_ids, list(range(min(seat_ids), max(seat_ids))))).pop()
+    visualise(seat_ids, your_seat)
+    return your_seat
 
 
 if __name__ == '__main__':
     print(part1())
     print(part2())
+    input("Press any key...")
--- Utils/graphics_panel.py
+++ Utils/graphics_panel.py
@@ -3,20 +3,21 @@ from Utils.point import Point
 
 
 class GraphicsPanel:
-    def __init__(self, tiles):
+    def __init__(self, tiles, scale):
         self.tiles = tiles
         self.rects = {}
         self.root = tk.Tk()
         self.canvas = None
         self.text_component = None
+        self.scale = scale
 
     @staticmethod
-    def create_empty_panel(x_dim, y_dim):
+    def create_empty_panel(x_dim, y_dim, scale):
         tile_map = {}
         for x in range(0, x_dim):
             for y in range(0, y_dim):
                 tile_map[Point(x, y)] = "black"
-        return GraphicsPanel(tile_map)
+        return GraphicsPanel(tile_map, scale)
 
     def reset(self):
         for point in self.tiles.keys():
@@ -29,9 +30,9 @@ class GraphicsPanel:
     def update_canvas(self, point, color):
         rect = self.rects.get(point)
         if rect is None:
-            rect = self.canvas.create_rectangle(point.x * GAME_SCALE, point.y * GAME_SCALE,
-                                                point.x * GAME_SCALE + GAME_SCALE,
-                                                point.y * GAME_SCALE + GAME_SCALE, fill=color)
+            rect = self.canvas.create_rectangle(point.x * self.scale, point.y * self.scale,
+                                                point.x * self.scale + self.scale,
+                                                point.y * self.scale + self.scale, fill=color)
             self.rects[point] = rect
         else:
             self.canvas.itemconfigure(rect, fill=color)
@@ -41,8 +42,8 @@ class GraphicsPanel:
         self.root.update()
 
     def init_canvas(self):
-        x_dimension = (max(list(map(lambda position: position.x, self.tiles.keys()))) + 1) * GAME_SCALE
-        y_dimension = (max(list(map(lambda position: position.y, self.tiles.keys()))) + 1) * GAME_SCALE
+        x_dimension = (max(list(map(lambda position: position.x, self.tiles.keys()))) + 1) * self.scale
+        y_dimension = (max(list(map(lambda position: position.y, self.tiles.keys()))) + 1) * self.scale
 
         self.canvas = tk.Canvas(self.root, width=x_dimension, height=y_dimension)
         self.canvas.pack()
@@ -52,12 +53,9 @@ class GraphicsPanel:
             self.update_canvas(point, color)
 
     def add_text(self, text, color):
-        self.text_component = self.canvas.create_text(GAME_SCALE * 2, GAME_SCALE * 2, fill=color,
-                                font="Courier " + str(GAME_SCALE - GAME_SCALE // 2), anchor="w",
+        self.text_component = self.canvas.create_text(self.scale * 2, self.scale * 2, fill=color,
+                                font="Courier " + str(self.scale - self.scale // 2), anchor="w",
                                 text=text)
 
     def update_text(self, text):
         self.canvas.itemconfigure(self.text_component, text=text)
-
-
-GAME_SCALE = 20
