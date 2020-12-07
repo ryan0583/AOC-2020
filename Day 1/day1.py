10d2dfa3310fbc287f7727836962c681eb5baf51
Ryan Griffiths <>
2020-12-07 20:03:30 +0000

    Day 1 using combinations

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,24 +1,23 @@
 from Utils.file_reader import read_lines
 
+from itertools import combinations
+
 
 def remainder_in_list(number, lines):
     return (2020 - int(number)) in lines
 
 
 def part1():
-    lines = list(map(int, read_lines()))
-    match = next(filter(lambda number: remainder_in_list(number, lines), lines))
+    lines = {int(line) for line in read_lines()}
+    match = {number for number in lines if remainder_in_list(number, lines)}.pop()
     return match * (2020 - match)
 
 
 def part2():
-    lines = list(map(int, read_lines()))
-
-    for line1 in lines:
-        for line2 in lines:
-            sum2 = int(line1) + int(line2)
-            if remainder_in_list(sum2, lines):
-                return line1 * line2 * (2020 - sum2)
+    lines = {int(line) for line in read_lines()}
+    combos = list(combinations(lines, 2))
+    pair = {pair for pair in combos if remainder_in_list(pair[0] + pair[1], lines)}.pop()
+    return pair[0] * pair[1] * (2020 - pair[0] - pair[1])
 
 
 print(part1())
