d698a6f656ded08ccc80b8cdecaadb4df9d54596
Ryan Griffiths <ryan.griffiths@itv.com>
2020-12-15 07:56:19 +0000

    Day 15 simplified

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -1,31 +1,22 @@
 from Utils.file_reader import read_lines
 
 
-def process(max):
+def process(upper):
     def take_turn():
-        print("TURN: " + str(turn))
-        # print("LAST: " + str(last_num))
-
         if turn < len(start_nums):
             next_num = start_nums[turn]
-        elif last_num in num_turn_map.keys() and len(num_turn_map[last_num]) > 1:
-            turns_seen = num_turn_map[last_num]
-            next_num = turns_seen[-1] - turns_seen[-2]
         else:
-            next_num = 0
+            next_num = turn - 1 - num_turn_map[last_num] if last_num in num_turn_map else 0
 
-        if next_num not in num_turn_map.keys():
-            num_turn_map[next_num] = []
-        num_turn_map[next_num].append(turn)
+        num_turn_map[last_num] = turn - 1
 
-        # print("=============")
         return next_num
 
     start_nums = [int(num) for num in read_lines("Input.txt")[0].split(",")]
 
-    last_num = -1
+    last_num = ""
     num_turn_map = {}
-    for turn in range(0, max):
+    for turn in range(0, upper):
         last_num = take_turn()
 
     return last_num
@@ -40,4 +31,4 @@ def part2():
 
 
 print(part1())
-# print(part2())
+print(part2())
