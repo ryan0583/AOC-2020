c85a20a6424d65213583db520f1101580cdaf00b
Ryan Griffiths <ryan.griffiths@itv.com>
2020-12-17 09:30:19 +0000

    Day 16 tidy

--- Day 16/day16.py
+++ Day 16/day16.py
@@ -1,3 +1,5 @@
+import math
+
 from Utils.file_reader import read_chunks
 
 
@@ -42,6 +44,35 @@ def part1():
 
 
 def part2():
+    def populate_possible_field_map():
+        for position in range(0, len(number_rows_ints[0])):
+            all_possible_fields = set()
+            for number in [number_row[position] for number_row in number_rows_ints]:
+                this_number_fields = []
+                for valid_row in valids:
+                    field_name_and_ranges = get_valid_range(valid_row)
+                    ranges = field_name_and_ranges[1]
+                    if int(ranges[0][0]) <= int(number) <= int(ranges[0][1]) \
+                            or int(ranges[1][0]) <= int(number) <= int(ranges[1][1]):
+                        this_number_fields.append(field_name_and_ranges[0])
+
+                if len(all_possible_fields) == 0:
+                    all_possible_fields = set(this_number_fields)
+                else:
+                    all_possible_fields = set.intersection(all_possible_fields, this_number_fields)
+
+                position_to_field_names[position] = list(all_possible_fields)
+
+    def remove_definitely_known_fields():
+        for key in position_to_field_names.keys():
+            if len(position_to_field_names[key]) == 1:
+                for other_key in position_to_field_names.keys():
+                    if key == other_key:
+                        continue
+                    position_to_field_names[other_key] = [entry
+                                                          for entry in position_to_field_names[other_key]
+                                                          if entry != position_to_field_names[key][0]]
+
     chunks = read_chunks('Input.txt')
     valids = chunks[0].split('\n')
     number_rows = chunks[2].split('\n')[1:]
@@ -54,48 +85,18 @@ def part2():
     number_rows_ints = [row.split(',') for row in remaining_number_rows]
 
     position_to_field_names = {}
-
-    for position in range(0, len(number_rows_ints[0])):
-        all_possible_fields = set()
-        for number in [number_row[position] for number_row in number_rows_ints]:
-            this_number_fields = []
-            for valid_row in valids:
-                field_name_and_ranges = get_valid_range(valid_row)
-                ranges = field_name_and_ranges[1]
-                for this_range in ranges:
-                    if int(this_range[0]) <= int(number) <= int(this_range[1]):
-                        this_number_fields.append(field_name_and_ranges[0])
-
-            if len(all_possible_fields) == 0:
-                all_possible_fields = set(this_number_fields)
-            else:
-                all_possible_fields = set.intersection(all_possible_fields, this_number_fields)
-
-            # add the list of possible fields to the map for this position
-            position_to_field_names[position] = list(all_possible_fields)
+    populate_possible_field_map()
 
     # Now go through the map, find positions for which we definitely know the field, and remove that
-    # field from all the other positions. Keep doing that until all position only have one possible field
+    # field from all the other positions. Keep doing that until all positions have only one possible field
     while len([fields for fields in position_to_field_names.values() if len(fields) > 1]) > 0:
-        for key in position_to_field_names.keys():
-            if len(position_to_field_names[key]) == 1:
-                for other_key in position_to_field_names.keys():
-                    if key == other_key:
-                        continue
-                    position_to_field_names[other_key] = [entry
-                                                          for entry in position_to_field_names[other_key]
-                                                          if entry != position_to_field_names[key][0]]
+        remove_definitely_known_fields()
 
     departure_positions = [key for key in position_to_field_names if 'departure' in position_to_field_names[key][0]]
 
     my_rows = [int(num_str) for num_str in chunks[1].split('\n')[1:][0].split(',')]
 
-    result = 1
-    for index, num in enumerate(my_rows):
-        if index in departure_positions:
-            result *= num
-
-    return result
+    return math.prod([num for index, num in enumerate(my_rows) if index in departure_positions])
 
 
 print(part1())
