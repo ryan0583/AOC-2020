6b04c515454097ed8a339213c5da1f2796702aae
Ryan Griffiths <ryan.griffiths@itv.com>
2020-12-23 20:45:30 +0000

    Day 23 cleanup

--- Day 23/day23.py
+++ Day 23/day23.py
@@ -13,13 +13,9 @@ class Entry:
         return self.value
 
 
-def move(current_cup, max_cup_value, cup_map, cup_one):
-    # print('CURRENT CUP: ' + str(current_cup.value))
-    # print(current_cup.next_cup.value)
-    # print(current_cup.next_cup.next_cup.value)
-    # print(current_cup.next_cup.next_cup.next_cup.value)
-
-    pickup_cups = [current_cup.next_cup.value, current_cup.next_cup.next_cup.value, current_cup.next_cup.next_cup.next_cup.value]
+def move(current_cup, max_cup_value, cup_map):
+    pickup_cups = [current_cup.next_cup.value, current_cup.next_cup.next_cup.value,
+                   current_cup.next_cup.next_cup.next_cup.value]
 
     dest_cup_value = current_cup.value - 1
     while dest_cup_value in pickup_cups or dest_cup_value <= 0:
@@ -29,54 +25,39 @@ def move(current_cup, max_cup_value, cup_map, cup_one):
 
     dest_cup = cup_map[dest_cup_value]
 
-    # print(dest_cup.value)
-
     removed_cup = current_cup.next_cup
 
-    current_cup.set_next_cup(
-        current_cup.next_cup.next_cup.next_cup.next_cup)
+    current_cup.set_next_cup(current_cup.next_cup.next_cup.next_cup.next_cup)
 
     removed_cup.next_cup.next_cup.set_next_cup(dest_cup.next_cup)
     dest_cup.set_next_cup(removed_cup)
 
-    # next_cup = cup_one.next_cup
-    # result = ''
-    # while next_cup.value != 1:
-    #     result += str(next_cup.value)
-    #     next_cup = next_cup.next_cup
-    #
-    # print(result)
-    # print('===================')
-
     return current_cup.next_cup
+
+
+def populate_links(line):
+    for index, entry in enumerate(line):
+        entry.set_next_cup(line[(index + 1) % len(line)])
+
+
+def play_game(line, rounds):
+    current_cup = line[0]
+    max_cup_value = max([e.value for e in line])
+    cup_map = {e.value: e for e in line}
+
+    for i in range(0, rounds):
+        # print('MOVE: ' + str(i + 1))
+        current_cup = move(current_cup, max_cup_value, cup_map)
 
 
 def part1():
     line = [Entry(int(c)) for c in read_lines('Input.txt')[0]]
 
-    for index, entry in enumerate(line):
-        entry.set_next_cup(line[(index + 1) % len(line)])
+    populate_links(line)
+
+    play_game(line, 100)
 
     cup_one = [cup for cup in line if cup.value == 1][0]
-    current_cup = line[0]
-    max_cup_value = max([e.value for e in line])
-    cup_map = {e.value: e for e in line}
-
-    # print(line[0].value)
-    # print(line[0].next_cup.value)
-
-    next_cup = cup_one.next_cup
-    result = ''
-    while next_cup.value != 1:
-        result += str(next_cup.value)
-        next_cup = next_cup.next_cup
-
-    print(result)
-
-    for i in range(0, 100):
-        print('MOVE: ' + str(i + 1))
-        current_cup = move(current_cup, max_cup_value, cup_map, cup_one)
-
     next_cup = cup_one.next_cup
     result = ''
     while next_cup.value != 1:
@@ -94,27 +75,16 @@ def part2():
     for i in range(max_num + 1, 1000001):
         line.append(Entry(i))
 
-    for index, entry in enumerate(line):
-        entry.set_next_cup(line[(index + 1) % len(line)])
+    populate_links(line)
+
+    play_game(line, 10000000)
 
     cup_one = [cup for cup in line if cup.value == 1][0]
-    current_cup = line[0]
-
-    max_cup_value = max([e.value for e in line])
-    cup_map = {e.value: e for e in line}
-
-    # print(line[0].value)
-    # print(line[0].next_cup.value)
-
-    for i in range(0, 10000000):
-        print('MOVE: ' + str(i + 1))
-        current_cup = move(current_cup, max_cup_value, cup_map, cup_one)
-
     result = cup_one.next_cup.value * cup_one.next_cup.next_cup.value
-    print(cup_one.next_cup.value)
-    print(cup_one.next_cup.next_cup.value)
+    # print(cup_one.next_cup.value)
+    # print(cup_one.next_cup.next_cup.value)
     print(result)
 
 
-# part1()
+part1()
 part2()
